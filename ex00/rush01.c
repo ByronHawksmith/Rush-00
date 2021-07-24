50a705bb9936c58b48f5274f4354d317a38acd7a
Byron Hawksmith <bhawksmi@F2N038.42adel.org.au>
2021-07-24 16:43:20 +0930

    Partial solution for rush01.c

--- ex00/rush01.c
+++ ex00/rush01.c
@@ -7,35 +7,64 @@ void	print_line(int line_len, char symbols[])
 	i = 0;
 	while (i < line_len)
 	{
-		if (i == 0 || i == (line_len - 1))
-			ft_putchar(symbols[0]);
+		if (i == 0)
+			ft_putchar(symbols[0]);	
+		else if (i == (line_len - 1))
+			ft_putchar(symbols[2]);
 		else
 			ft_putchar(symbols[1]);
 		i++;
 	}
 }
 
-void	rush(int x, int y)
+void	logic(int x, int y, char horiz_symb[], char vert_symb[])
 {
 	int		i;
-	char	horiz_symb[2];
-	char	vert_symb[2];
+	char	symbols[3];
 
 	i = y;
-	horiz_symb[0] = 'o';
-	horiz_symb[1] = '-';
-	vert_symb[0] = '|';
-	vert_symb[1] = ' ';
 	if (x + y == 1)
 		return ;
 	while (i >= 1)
 	{
-		if (i == 1 || i == y)
-			print_line(x, horiz_symb);
+		if (i == 1)
+		{
+			symbols[0] = horiz_symb[2];
+			symbols[1] = horiz_symb[4];
+			symbols[2] = horiz_symb[3];
+			print_line(x, symbols);
+		}
+		else if (i == y)
+		{
+			symbols[0] = horiz_symb[0];
+			symbols[1] = horiz_symb[4];
+			symbols[2] = horiz_symb[1];
+			print_line(x, symbols);
+		}
 		else
-			print_line(x, vert_symb);
+		{
+			symbols[0] = vert_symb[0];
+			symbols[1] = vert_symb[1];
+			symbols[2] = vert_symb[0];
+			print_line(x, symbols);
+		}
 		if (i != 1)
 			ft_putchar('\n');
 		i--;
 	}
+}
+
+void	rush(int x, int y)
+{
+	char	horiz_symb[5];
+	char	vert_symb[2];
+
+	horiz_symb[0] = '/';
+	horiz_symb[1] = '\\';
+	horiz_symb[2] = '\\';
+	horiz_symb[3] = '/';
+	horiz_symb[4] = '*';
+	vert_symb[0] = '*';
+	vert_symb[1] = ' ';
+	logic(x, y, horiz_symb, vert_symb);
 }
