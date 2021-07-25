311fcdfa71ce66b5cde9e38315e1c8d663edd403
Byron Hawksmith <bhawksmi@F2N038.42adel.org.au>
2021-07-25 11:44:23 +0930

    Change rush00.c to be the same as other rush files

--- ex00/rush00.c
+++ ex00/rush00.c
@@ -1,41 +1,113 @@
 void	ft_putchar(char c);
 
-void	print_line(int line_len, char symbols[])
+/*
+ * Determine which characters to use based on
+ * a given mode.
+ *
+ * Mode 0: last row
+ * Mode 1: first row
+ * Mode 2: middle row
+ */
+void	determine_chars(int mode, char *chars)
+{
+	chars[2] = '-';
+	if (mode == 0)
+	{
+		chars[0] = 'o';
+		chars[1] = 'o';
+	}
+	else if (mode == 1)
+	{
+		chars[0] = 'o';
+		chars[1] = 'o';
+	}
+	else
+	{
+		chars[0] = '|';
+		chars[1] = '|';
+		chars[2] = ' ';
+	}
+}
+
+/*
+ * Print a given row by looping over an int
+ * row_len.
+ *
+ * Print the appropriate char based on if
+ * the character is in the last, first or
+ * middle position.
+ */
+void	print_line(int line_len, char chars[])
 {
 	int	i;
 
 	i = 0;
 	while (i < line_len)
 	{
-		if (i == 0 || i == (line_len - 1))
-			ft_putchar(symbols[0]);
+		if (i == 0)
+			ft_putchar(chars[0]);
+		else if (i == (line_len - 1))
+			ft_putchar(chars[1]);
 		else
-			ft_putchar(symbols[1]);
+			ft_putchar(chars[2]);
 		i++;
 	}
 }
 
-void	rush(int x, int y)
+/*
+ * Print all rows by looping over an int
+ * i which is initially set to y: the
+ * number of rows.
+ *
+ * Determine the characters to use based on
+ * if the current row is the the first, last
+ * or a middle row.
+ *
+ * Exceptions:
+ *
+ * If the dimensions are invalid:
+ * i.e. (0, 0), (0, 1), (1, 0), print nothing.
+ *
+ * If printing the last row, don't print a \n
+ * char.
+ */
+void	logic(int x, int y, char chars[])
 {
 	int		i;
-	char	horiz_symb[2];
-	char	vert_symb[2];
 
 	i = y;
-	horiz_symb[0] = 'o';
-	horiz_symb[1] = '-';
-	vert_symb[0] = '|';
-	vert_symb[1] = ' ';
 	if (x + y == 1)
 		return ;
 	while (i >= 1)
 	{
-		if (i == 1 || i == y)
-			print_line(x, horiz_symb);
+		if (i == 1)
+			determine_chars(0, chars);
+		else if (i == y)
+			determine_chars(1, chars);
 		else
-			print_line(x, vert_symb);
+			determine_chars(2, chars);
+		print_line(x, chars);
 		if (i != 1)
 			ft_putchar('\n');
 		i--;
+	}
+}
+
+/*
+ * If we only have one row print it with the
+ * first row mode. Otherwise do normal logic.
+ */
+void	rush(int x, int y)
+{
+	char	chars[3];
+
+	if (y == 1)
+	{
+		determine_chars(1, chars);
+		print_line(x, chars);
+	}
+	else
+	{
+		logic(x, y, chars);
 	}
 }
